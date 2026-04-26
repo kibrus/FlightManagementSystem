7a4f32eea51295b17a02bc01173cc2b41a49ef4b
Kibru <kibrussmenore@gmail.com>
2026-04-25 21:38:25 -0500

    Fix src folder tracking

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+/**
+ * Main.java
+ * Entry point for the Flight Management System
+ * Team: [Your team names here]
+ * Date: April 2026
+ */
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("kibru Flight Management System...");
+    }
+}
